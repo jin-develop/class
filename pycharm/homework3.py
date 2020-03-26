e342d30e2d3a4c631b5397d2e2461878d97b865c
jin-develop <jin-develop@users.noreply.github.com>
2020-03-26 14:32:49 +0900

    pycharm +

---
+++ pycharm/homework3.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
+
+#############################
+# (입맛에 맞게 코딩)
+#############################
+songs = soup.select('tr.list')
+rank = 0
+for song in songs:
+    rank += 1
+    name = song.select('tr > td.info > a')
+    song_name = name[0].text.strip()
+    artist = name[1].text
+    print('*'*70)
+    print('{}   {}   {}'.format(rank, song_name, artist))
+
+# for song in songs:
+#     name = song.select_one('')
+
